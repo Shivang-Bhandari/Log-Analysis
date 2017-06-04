68340093213cebed143f6b0570b85552fd278c57
Shivang Bhandari <pump382156@gmail.com>
2017-06-04 23:29:05 +0530

    functions modified

--- logparser.py
+++ logparser.py
@@ -3,10 +3,22 @@
 
 import psycopg2
 
-def parseLog(query):
-    database = psycopg2.connect("dbname=news")
+def dbConnect():
+    #error handling for connection to DB
+    try:
+        database = psycopg2.connect("dbname=news")
+    else:
+        "Couldn't connect to DB"
+
+    #initialized cursor to Db
     dbCursor = database.cursor()
+
+
+def parseLog(query):
     dbCursor.execute(query)
     parsedData = dbCursor.fetchall()
     database.close()
     return parsedData
+
+#connection call to DB
+dbConnect()
