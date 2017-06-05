ce6bf44caa8088560283c1aa3ff3a3363cc0eb80
Shivang Bhandari <pump382156@gmail.com>
2017-06-05 15:30:47 +0530

    first query running

--- logparser.py
+++ logparser.py
@@ -2,12 +2,16 @@
 
 import psycopg2
 
+queries=["select title,hits from articleHits limit 3"]
 
 def dbConnect():
+
+    global database
+    global dbCursor
     # error handling for connection to DB
     try:
         database = psycopg2.connect("dbname=news")
-    else:
+    except:
         print "Couldn't connect to DB"
 
     # initialized cursor to Db
@@ -20,5 +24,15 @@ def parseLog(query):
     database.close()
     return parsedData
 
+def dataOutput(parsedData):
+    print parsedData['title']
+    for data in parsedData['data']:
+        print '\t'+str(data[0])+' => '+str(data[1])+' hits'
+
+
 # connection call to DB
 dbConnect()
+query1 = dict()
+query1['title']="The Top 3 Articles are :"
+query1['data']=parseLog(queries[0])
+dataOutput(query1)
