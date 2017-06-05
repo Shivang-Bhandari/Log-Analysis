80e7a7e0a2118b9967bda2b6091cd9d55a7fb7a2
Shivang Bhandari <pump382156@gmail.com>
2017-06-06 03:09:30 +0530

    query 3 running

--- logparser.py
+++ logparser.py
@@ -3,7 +3,15 @@
 import psycopg2
 
 queries=["select title,hits from articleHits limit 3",
-        "select name,sum(articleHits.hits) as num from authors, articleHits where authors.id = articleHits.author group by name order by num desc;"]
+        "select name,sum(articleHits.hits) as num from authors, articleHits where authors.id = articleHits.author group by name order by num desc;",
+        """select currentDate, round(errorCount*100.0/(okCount+errorCount),2)
+           as errorprc
+           from(
+           select date(time) as currentDate,
+           sum(case when status='200 OK' then 1 else 0 END) as okCount,
+           sum(case when status='404 NOT FOUND' then 1 else 0 END) as errorCount
+           from log group by date(time)
+           ) as derived_table where errorCount*100/(okCount+errorCount)>1"""]
 
 def dbConnect():
 
@@ -27,7 +35,12 @@ def parseLog(query):
 def dataOutput(parsedData):
     print parsedData['title']
     for data in parsedData['data']:
-        print '\t'+str(data[0])+' => '+str(data[1])+' hits'
+        print '\t~ '+str(data[0])+' => '+str(data[1])+' hits'
+
+def dataOutputError(parsedData):
+    print parsedData['title']
+    for data in parsedData['data']:
+        print '\t~ '+str(data[0])+' => '+str(data[1])+' %'
 
 
 # connection call to DB
@@ -39,10 +52,10 @@ topArticles = {'title':"The Top 3 Articles are :", 'data':parseLog(queries[0])}
 topAuthors = {'title':"Top Authors are :", 'data':parseLog(queries[1])}
 
 # fetches error days
-# errorDays = {'title':"Days with more than 1 percent error are :", 'data':parseLog(queries[2])}
+errorDays = {'title':"Days with more than 1 percent error are :", 'data':parseLog(queries[2])}
 
-# Output on
+# Output
 dataOutput(topArticles)
 dataOutput(topAuthors)
-# dataOutput(errorDays)
+dataOutputError(errorDays)
 database.close()
