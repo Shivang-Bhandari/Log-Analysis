1309e5cbf8d2102b4641d80c266c3d3f082eb8e3
Shivang Bhandari <pump382156@gmail.com>
2017-06-06 03:19:41 +0530

    pep8 formatting

--- logparser.py
+++ logparser.py
@@ -4,18 +4,23 @@ import psycopg2
 
 # list of queries to run on the DB
 
-queries=["select title,hits from articleHits limit 3",
-        "select name,sum(articleHits.hits) as num from authors, articleHits where authors.id = articleHits.author group by name order by num desc;",
-        """select currentDate, round(errorCount*100.0/(okCount+errorCount),2)
+queries = ["select title,hits from articleHits limit 3",
+           """select name,sum(articleHits.hits) as num from authors,
+           articleHits where authors.id = articleHits.author
+           group by name order by num desc;""",
+           """select currentDate, round(errorCount*100.0/(okCount+errorCount),2)
            as errorprc
            from(
            select date(time) as currentDate,
-           sum(case when status='200 OK' then 1 else 0 END) as okCount,
-           sum(case when status='404 NOT FOUND' then 1 else 0 END) as errorCount
+           sum(case when status='200 OK' then 1 else 0 END)
+           as okCount,
+           sum(case when status='404 NOT FOUND' then 1 else 0 END)
+           as errorCount
            from log group by date(time)
            ) as derived_table where errorCount*100/(okCount+errorCount)>1"""]
 
 # function to connect to the DB
+
 
 def dbConnect():
 
@@ -31,16 +36,20 @@ def dbConnect():
     dbCursor = database.cursor()
 
 # function to generate data by running queries on DB
+
+
 def parseLog(query):
     dbCursor.execute(query)
     parsedData = dbCursor.fetchall()
     return parsedData
+
 
 # function to print out data for first two queries
 def dataOutput(parsedData):
     print parsedData['title']
     for data in parsedData['data']:
         print '\t~ '+str(data[0])+' => '+str(data[1])+' hits'
+
 
 # function to print data for %error query
 def dataOutputError(parsedData):
@@ -53,13 +62,13 @@ def dataOutputError(parsedData):
 dbConnect()
 
 # fetches top articles
-topArticles = {'title':"The Top 3 Articles are :", 'data':parseLog(queries[0])}
+topArticles = {'title': "The Top 3 Articles are : ", 'data': parseLog(queries[0])}
 
 # fetches top authors
-topAuthors = {'title':"Top Authors are :", 'data':parseLog(queries[1])}
+topAuthors = {'title': "Top Authors are :", 'data': parseLog(queries[1])}
 
 # fetches error days
-errorDays = {'title':"Days with more than 1 percent error are :", 'data':parseLog(queries[2])}
+errorDays = {'title': "Days with more than 1 percent error are :", 'data': parseLog(queries[2])}
 
 # Output
 dataOutput(topArticles)
